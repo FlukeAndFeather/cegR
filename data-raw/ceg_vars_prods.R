830ae1795d24eb4ecfe8a2d8048d08cfbcd8bd35
Max Czapanskiy <maxczapanskiy@gmail.com>
2023-11-15 09:31:33 -0800

    Remove derived variables and read variable metadata directly from google sheets (fix #10)

--- data-raw/ceg_vars_prods.R
+++ data-raw/ceg_vars_prods.R
@@ -1,8 +1,13 @@
 ## Products and variables used by CEG Heritage Harbor
 
+# Authenticate if necessary
+# googlesheets4::gs4_auth(email = "*@noaa.gov")
+
 # These are internal
-ceg_product_tbl <- readr::read_csv(here::here("data-raw/CEG Geospatial Data Sources - Products.csv"))
-ceg_variable_tbl <- readr::read_csv(here::here("data-raw/CEG Geospatial Data Sources - Variables.csv"))
+ceg_product_tbl <- googlesheets4::read_sheet("15duOwuf5PCRfLEXWJzopdzQbZkjHhTvHbop2xDWWcME",
+                                             "Products")
+ceg_variable_tbl <- googlesheets4::read_sheet("15duOwuf5PCRfLEXWJzopdzQbZkjHhTvHbop2xDWWcME",
+                                              "Variables")
 
 # External-facing "ceg_vars" object takes the form:
 # ceg_vars$<product_name>$<variable_name>$<time_scale> and the value is the
